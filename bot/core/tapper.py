0b315b60b1bd43782f3ae6f1fd2b84e455be7c20
shamhi <shomakakdela@gmail.com>
2024-05-21 21:49:14 +0300

    📍 Optimize code

--- bot/core/tapper.py
+++ bot/core/tapper.py
@@ -81,8 +81,7 @@ class Tapper:
             return access_token
         except Exception as error:
             logger.error(f"{self.session_name} | Unknown error while getting Access Token: {error}")
-            logger.error(f"{self.session_name} | Sleep for 3 min(180 sec)")
-            await asyncio.sleep(delay=180)
+            await asyncio.sleep(delay=3)
 
     async def get_profile_data(self, http_client: aiohttp.ClientSession) -> dict[str]:
         try:
@@ -96,8 +95,7 @@ class Tapper:
             return profile_data
         except Exception as error:
             logger.error(f"{self.session_name} | Unknown error while getting Profile Data: {error}")
-            logger.error(f"{self.session_name} | Sleep for 3 min(180 sec)")
-            await asyncio.sleep(delay=180)
+            await asyncio.sleep(delay=3)
 
     async def get_tasks(self, http_client: aiohttp.ClientSession) -> dict[str]:
         try:
@@ -252,7 +250,7 @@ class Tapper:
                             status = await self.get_daily(http_client=http_client)
                             if status is True:
                                 logger.success(f"{self.session_name} | Successfully get daily reward | "
-                                               f"Days: <m>{days}</m> | Reward coins: {rewards[days-1]['rewardCoins']}")
+                                               f"Days: <m>{days}</m> | Reward coins: {rewards[days - 1]['rewardCoins']}")
 
                     taps = randint(a=settings.RANDOM_TAPS_COUNT[0], b=settings.RANDOM_TAPS_COUNT[1])
 
@@ -277,7 +275,7 @@ class Tapper:
                     earn_on_hour = player_data['earnPassivePerHour']
 
                     boosts = player_data['boosts']
-                    energy_boost_time = boosts.get('BoostFullAvailableTaps', {}).get('lastUpgradeAt', 0)
+                    energy_boost = boosts.get('BoostFullAvailableTaps', {})
 
                     logger.success(f"{self.session_name} | Successful tapped! | "
                                    f"Balance: <c>{balance}</c> (<g>+{calc_taps}</g>) | Total: <e>{total}</e>")
@@ -285,7 +283,8 @@ class Tapper:
                     if active_turbo is False:
                         if (settings.APPLY_DAILY_ENERGY is True
                                 and available_energy < settings.MIN_AVAILABLE_ENERGY
-                                and time() - energy_boost_time > 3600):
+                                and energy_boost.get("cooldownSeconds", 0) == 0
+                                and energy_boost.get("level", 0) <= energy_boost.get("maxLevel", 0)):
                             logger.info(f"{self.session_name} | Sleep 5s before apply energy boost")
                             await asyncio.sleep(delay=5)
 
@@ -295,34 +294,35 @@ class Tapper:
 
                                 await asyncio.sleep(delay=1)
 
-                            continue
+                                continue
 
                         if settings.AUTO_UPGRADE is True:
                             upgrades = await self.get_upgrades(http_client=http_client)
-                            available_upgrades = [data for data in upgrades if data['isAvailable'] is True and data['isExpired'] is False]
+                            available_upgrades = [data for data in upgrades if
+                                                  data['isAvailable'] is True and data['isExpired'] is False]
                             queue = []
-                        
+
                             for upgrade in available_upgrades:
                                 upgrade_id = upgrade['id']
                                 level = upgrade['level']
                                 price = upgrade['price']
                                 profit = upgrade['profitPerHourDelta']
-                        
-                                significance = profit /  price
-                        
+
+                                significance = profit / price
+
                                 if balance > price and level <= settings.MAX_LEVEL:
                                     queue.append([upgrade_id, significance, level, price, profit])
-                            
+
                             # sort by significance
                             queue.sort(key=operator.itemgetter(1), reverse=True)
-                        
+
                             for upgrade in queue:
                                 if balance > upgrade[3] and upgrade[2] <= settings.MAX_LEVEL:
                                     logger.info(f"{self.session_name} | Sleep 5s before upgrade <e>{upgrade[0]}</e>")
                                     await asyncio.sleep(delay=5)
-                                    
+
                                     status = await self.buy_upgrade(http_client=http_client, upgrade_id=upgrade[0])
-                        
+
                                     if status is True:
                                         earn_on_hour += upgrade[4]
                                         balance -= upgrade[3]
@@ -330,9 +330,9 @@ class Tapper:
                                             f"{self.session_name} | "
                                             f"Successfully upgraded <e>{upgrade[0]}</e> to <m>{upgrade[2]}</m> lvl | "
                                             f"Earn every hour: <y>{earn_on_hour}</y> (<g>+{upgrade[4]}</g>)")
-                            
+
                                         await asyncio.sleep(delay=1)
-                        
+
                                 continue
 
                         if available_energy < settings.MIN_AVAILABLE_ENERGY:
