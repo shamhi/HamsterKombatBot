0d17c6616d5795aed3b29caacff2f9ac65ee0b73
shamhi <shomakakdela@gmail.com>
2024-05-29 00:50:14 +0300

    🛠 Change logic

--- db/models.py
+++ db/models.py
@@ -35,7 +35,7 @@ class Logs(Base):
 class NextTimes(Base):
     __tablename__ = 'next_times'
 
-    account__ID = Column(BigInteger, ForeignKey("accounts.number"), nullable=False)
+    account__ID = Column(BigInteger, nullable=False)
 
     tap = Column(BigInteger, nullable=True, default=None)
     claim = Column(BigInteger, nullable=True, default=None)
