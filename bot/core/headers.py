774b32dbbd5afeecf2a567a13b4b674e56693f5d
shamhi <shomakakdela@gmail.com>
2024-06-07 22:11:30 +0300

    📕 Edit headers

--- bot/core/headers.py
+++ bot/core/headers.py
@@ -11,5 +11,5 @@ headers = {
     'Sec-Ch-Ua': '"Google Chrome";v="125", "Chromium";v="125", "Not.A/Brand";v="24"',
     'Sec-Ch-Ua-mobile': '?1',
     'Sec-Ch-Ua-platform': '"Android"',
-    'User-Agent': 'Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/125.0.0.0 Mobile Safari/537.36',
+    'User-Agent': 'Mozilla/5.0 (Linux; Android 14) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/125.0.6422.165 Mobile Safari/537.36',
 }
