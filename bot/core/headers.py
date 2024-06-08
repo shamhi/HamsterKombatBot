4b6d310993cda95c8df916b6c17ae40986fd9eb7
4erdenko <4erdenko@tuta.io>
2024-06-08 11:36:01 +0300

    Update headers with dynamic User-Agent values
    Refactored headers.py to generate User-Agent and Sec-Ch-Ua values dynamically. Added two functions: get_mobile_user_agent for generating random User-Agent with 'mobile' platform, and get_headers for constructing the headers with random Sec-Ch-Ua values. These changes will help to mimic different browser requests more realistically.

--- bot/core/headers.py
+++ bot/core/headers.py
@@ -1,15 +1,27 @@
-headers = {
-    'Accept': 'application/json',
-    'Accept-Language': 'ru-RU,ru;q=0.9',
-    'Content-Type': 'application/json',
-    'Connection': 'keep-alive',
-    'Origin': 'https://hamsterkombat.io',
-    'Referer': 'https://hamsterkombat.io/',
-    'Sec-Fetch-Dest': 'empty',
-    'Sec-Fetch-Mode': 'cors',
-    'Sec-Fetch-Site': 'same-site',
-    'Sec-Ch-Ua': '"Google Chrome";v="125", "Chromium";v="125", "Not.A/Brand";v="24"',
-    'Sec-Ch-Ua-mobile': '?1',
-    'Sec-Ch-Ua-platform': '"Android"',
-    'User-Agent': 'Mozilla/5.0 (Linux; Android 14) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/125.0.6422.165 Mobile Safari/537.36',
-}
+import random
+
+from fake_useragent import UserAgent
+
+
+def get_mobile_user_agent():
+    ua = UserAgent(platforms=['mobile'])
+    return ua.random
+
+
+def get_headers():
+    headers = {
+        'Accept': 'application/json',
+        'Accept-Language': 'ru-RU,ru;q=0.9',
+        'Content-Type': 'application/json',
+        'Connection': 'keep-alive',
+        'Origin': 'https://hamsterkombat.io',
+        'Referer': 'https://hamsterkombat.io/',
+        'Sec-Fetch-Dest': 'empty',
+        'Sec-Fetch-Mode': 'cors',
+        'Sec-Fetch-Site': 'same-site',
+        'Sec-Ch-Ua': f'"Google Chrome";v="{random.randint(70, 125)}", "Chromium";v="{random.randint(70, 125)}", "Not.A/Brand";v="{random.randint(10, 30)}"',
+        'Sec-Ch-Ua-mobile': '?1',
+        'Sec-Ch-Ua-platform': '"Android"',
+        'User-Agent': get_mobile_user_agent()
+    }
+    return headers
