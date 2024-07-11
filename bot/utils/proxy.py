8a331886419c1d72bd4653297cffcdb9092eeda0
shamhi <shomakakdela@gmail.com>
2024-07-11 17:58:26 +0300

    🔥 Update proxy

--- bot/utils/proxy.py
+++ bot/utils/proxy.py
@@ -1,6 +1,34 @@
 import aiohttp
+from better_proxy import Proxy
 
 from bot.utils.logger import logger
+from bot.utils.json_db import JsonDB
+
+
+def get_proxy_dict(proxy: str):
+    try:
+        proxy = Proxy.from_str(proxy=proxy.strip())
+
+        proxy_dict = dict(
+            scheme=proxy.protocol,
+            hostname=proxy.host,
+            port=proxy.port,
+            username=proxy.login,
+            password=proxy.password,
+        )
+
+        return proxy_dict
+    except ValueError:
+        return None
+
+
+def get_proxy_string(name: str):
+    db = JsonDB("profiles")
+
+    data = db.get_data()
+    proxy = data.get(name, {}).get("proxy", "")
+
+    return proxy
 
 
 async def check_proxy(
@@ -13,4 +41,4 @@ async def check_proxy(
         ip = (await response.json()).get('origin')
         logger.info(f'{session_name} | Proxy IP: {ip}')
     except Exception as error:
-        logger.error(f'{session_name} | Proxy: {proxy} | Error: {error}')
+        logger.error(f'{session_name} | Proxy: <le>{proxy}</le> | Error: <lr>{error}</lr>')
