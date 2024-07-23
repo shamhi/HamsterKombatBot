043e45734482e18e6c91bb944785f4c896f39ff0
shamhi <shomakakdela@gmail.com>
2024-07-23 19:54:37 +0300

    ⚙ Add new settings

--- bot/config/config.py
+++ bot/config/config.py
@@ -10,10 +10,9 @@ class Settings(BaseSettings):
     MIN_AVAILABLE_ENERGY: int = 100
     SLEEP_BY_MIN_ENERGY: list[int] = [1800, 2400]
 
-    ADD_TAPS_ON_TURBO: int = 2500
-
     AUTO_UPGRADE: bool = True
     MAX_LEVEL: int = 20
+    MAX_PRICE: int = 50000000
 
     BALANCE_TO_SAVE: int = 1000000
     UPGRADES_COUNT: int = 10
@@ -23,8 +22,12 @@ class Settings(BaseSettings):
     APPLY_DAILY_ENERGY: bool = True
     APPLY_DAILY_TURBO: bool = True
 
+    USE_TAPS: bool = True
     RANDOM_TAPS_COUNT: list[int] = [10, 50]
     SLEEP_BETWEEN_TAP: list[int] = [10, 25]
+
+    USE_RANDOM_DELAY_IN_RUN: bool = False
+    RANDOM_DELAY_IN_RUN: list[int] = [0, 15]
 
     USE_RANDOM_USERAGENT: bool = False
 
