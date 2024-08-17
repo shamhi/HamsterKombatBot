1311e8e675812dacde6e99d7992755c07220673c
Georgy <georgsh-yateam@yandex.com>
2024-08-18 02:21:24 +0300

    check python version file

---
+++ check_python.py
@@ -0,0 +1,5 @@
+import sys
+if sys.version_info.major != 3 or sys.version_info.minor < 11 or sys.version_info > 11:
+    print('Python version not supported:', sys.version)
+    print('\n\nInstall Python 3.11: https://www.python.org/downloads/')
+    sys.exit(1)
