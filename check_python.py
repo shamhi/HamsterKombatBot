98e55f175d116975a4ccb153f869304a61ba3030
Georgy <georgsh-yateam@yandex.com>
2024-08-18 05:32:03 +0300

    python 3.10 = ok

--- check_python.py
+++ check_python.py
@@ -1,5 +1,5 @@
 import sys
-if sys.version_info.major != 3 or sys.version_info.minor < 11 or sys.version_info > 11:
+if sys.version_info.major != 3 or sys.version_info.minor < 10 or sys.version_info > 11:
     print('Python version not supported:', sys.version)
     print('\n\nInstall Python 3.11: https://www.python.org/downloads/')
     sys.exit(1)
