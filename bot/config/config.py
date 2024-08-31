1ba2ba56d9a198c0740f3355441f282662f1002b
shamhi <shomakakdela@gmail.com>
2024-08-31 11:12:57 +0300

    ⚙ Revert USE_RANDOM_MINI_GAME_KEY

--- bot/config/config.py
+++ bot/config/config.py
@@ -30,6 +30,8 @@ class Settings(BaseSettings):
     SCORE_MINI_GAME_TILES: list[int] = [300, 500]
     GAMES_COUNT: list[int] = [1, 10]
 
+    USE_RANDOM_MINI_GAME_KEY: bool = True
+
     AUTO_COMPLETE_TASKS: bool = True
 
     USE_TAPS: bool = True
